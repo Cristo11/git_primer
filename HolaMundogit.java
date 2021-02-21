0d1d9b6658e6c9da9544395f4ae1bdacb5c3ad7c
Cristo Corujo <cristobal@gmail.com>
2021-02-21 13:56:57 +0000

    mi primer commit

---
+++ HolaMundogit.java
@@ -0,0 +1,14 @@
+package HolaMundogit;
+
+public class HolaMundogit {
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		System.out.print("HolaMundo");
+		System.out.println("jejejeje");
+		System.out.println("nota2");
+		}
+
+	}
+
+
+
